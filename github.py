47a3453d5d82bb1b17a0e45a26dfc97654048c70
Yoann <yoann9344@gmail.com>
2021-09-10 09:27:04 +0200

    Add helper, Use black

--- github.py
+++ github.py
@@ -4,6 +4,7 @@ import re
 import sys
 import tarfile
 from dataclasses import dataclass
+from inspect import cleandoc
 from os.path import normpath
 from os.path import join as path_join
 from pathlib import Path, PurePath
@@ -18,34 +19,35 @@ def is_path_secure(tar_info: tarfile.TarInfo, directory: str) -> bool:
     target_dir = Path(directory)
     joined_path = path_join(directory, tar_info.path)
     target_file = PurePath(normpath(joined_path))
-    if target_dir not in target_file.parents and target_dir.parent != target_file.parent:
+    if (
+        target_dir not in target_file.parents
+        and target_dir.parent != target_file.parent
+    ):
         if debug:
-            print(f'DEBUG SECURE {joined_path} will write data outside {target_dir}')
+            print(f"DEBUG SECURE {joined_path} will write data outside {target_dir}")
         return False
 
     # Same as above but for the symlink target
     if tar_info.issym() or tar_info.islnk():
         if debug:
-            print('DEBUG SECURE Is a link')
+            print("DEBUG SECURE Is a link")
         return False  # do not even consider ln
-        symlink_file = PurePath(normpath(
-            path_join(directory, tar_info.linkname)
-        ))
+        symlink_file = PurePath(normpath(path_join(directory, tar_info.linkname)))
         if target_dir not in symlink_file.parents:
             return False
     return True
 
 
 def handle_answer(answer: str) -> bool:
-    if answer == 'y':
+    if answer == "y":
         return True
-    elif answer == 'Y':
+    elif answer == "Y":
         global yes_man
         yes_man = True
         return True
-    elif answer == 'n':
+    elif answer == "n":
         return False
-    elif answer == 'N':
+    elif answer == "N":
         global no_no_no_no
         no_no_no_no = True
         return False
@@ -67,11 +69,11 @@ class Archive:
         nb_files_extracted = 0
         nb_dirs_extracted = 0
         if only_save_tar:
-            new_tar = tarfile.open(f'{self.repo_name}.tar.gz', mode='w|gz')
+            new_tar = tarfile.open(f"{self.repo_name}.tar.gz", mode="w|gz")
 
         for tarinfo in self.tar:
             if debug:
-                print(f'DEBUG path {tarinfo.path}')
+                print(f"DEBUG path {tarinfo.path}")
             if self.is_file:
                 is_in_path = self.archive_path == PurePath(tarinfo.path)
             else:
@@ -79,38 +81,36 @@ class Archive:
             if is_in_path:
                 # change path that will be created with extract's method
                 # to not create empty topdirs
-                tarinfo.path = str(
-                    PurePath(tarinfo.path).relative_to(self.archive_dir)
-                )
+                tarinfo.path = str(PurePath(tarinfo.path).relative_to(self.archive_dir))
                 if only_save_tar:
                     new_tar.addfile(tarinfo, self.tar.extractfile(tarinfo))
                     continue
                 elif not is_path_secure(tar_info=tarinfo, directory=directory):
                     continue
-                elif Path(f'{directory}/{tarinfo.path}').exists() and not yes_man:
+                elif Path(f"{directory}/{tarinfo.path}").exists() and not yes_man:
                     if no_no_no_no:
                         continue
-                    print('Extracting', tarinfo.name.split('/')[-1])
+                    print("Extracting", tarinfo.name.split("/")[-1])
                     validated = None
                     while validated is None:
                         validated = input(
-                            'This file already exist, wanna overide it ?'
-                            'y/n/Y/N (Y: yes all, N: no all) '
+                            "This file already exist, wanna overide it ?"
+                            "y/n/Y/N (Y: yes all, N: no all) "
                         )
-                        sys.stdout.write('\033[F\033[1G\033[K' * 2)
+                        sys.stdout.write("\033[F\033[1G\033[K" * 2)
                         validated = handle_answer(validated)
                     if not validated:
                         continue
 
                 if verbose:
-                    print('extracted', tarinfo.name.split('/')[-1])
+                    print("extracted", tarinfo.name.split("/")[-1])
                 self.tar.extract(tarinfo, path=directory)
                 if tarinfo.isdir():
                     nb_dirs_extracted += 1
                 elif tarinfo.isfile():
                     nb_files_extracted += 1
-        print(f'{nb_files_extracted} files extracted !')
-        print(f'{nb_dirs_extracted} directories extracted !')
+        print(f"{nb_files_extracted} files extracted !")
+        print(f"{nb_dirs_extracted} directories extracted !")
         if only_save_tar:
             new_tar.close()
         self.tar.close()
@@ -118,42 +118,41 @@ class Archive:
 
 def get_github_archive(url: str):
     archive_url, user, repo_name, is_file, branch, target_path = re.search(
-        r'(https://github\.com/([^/]+)/([^/]+)/(tree|blob)/([^/]+))/(.*)$',
-        url
+        r"(https://github\.com/([^/]+)/([^/]+)/(tree|blob)/([^/]+))/(.*)$", url
     ).groups()
-    is_file = is_file == 'blob'
+    is_file = is_file == "blob"
 
     # replace /tree/ or /blob/ by /archive/
     archive_url = re.sub(
-        r'(https://github\.com/[^/]+/[^/]+)/(tree|blob)/',
-        r'\1/archive/',
+        r"(https://github\.com/[^/]+/[^/]+)/(tree|blob)/",
+        r"\1/archive/",
         archive_url,
     )
-    archive_url += '.tar.gz'
-    first_dir_name = f'{repo_name}-{branch}'
+    archive_url += ".tar.gz"
+    first_dir_name = f"{repo_name}-{branch}"
     if is_file:
-        target_dir = '/'.join(target_path.split('/')[:-1]) + '/'
-        if target_dir == '/':
-            target_dir = './'
+        target_dir = "/".join(target_path.split("/")[:-1]) + "/"
+        if target_dir == "/":
+            target_dir = "./"
         archive_path = PurePath(first_dir_name) / target_path
         archive_dir = PurePath(first_dir_name) / target_dir
     else:
         archive_path = PurePath(first_dir_name) / target_path
         archive_dir = archive_path
     if debug:
-        if Path('debug.tar.gz').exists():
-            tar = tarfile.open('debug.tar.gz', 'r|gz')
+        if Path("debug.tar.gz").exists():
+            tar = tarfile.open("debug.tar.gz", "r|gz")
         else:
             response = requests.get(archive_url)
             tar = tarfile.open(
                 fileobj=io.BytesIO(response.content),
-                mode='r|gz',
+                mode="r|gz",
             )
-            with open('debug.tar.gz', 'wb+') as file:
+            with open("debug.tar.gz", "wb+") as file:
                 file.write(response.content)
     else:
         response = requests.get(archive_url)
-        tar = tarfile.open(fileobj=io.BytesIO(response.content), mode='r|gz')
+        tar = tarfile.open(fileobj=io.BytesIO(response.content), mode="r|gz")
 
     archive = Archive(
         tar,
@@ -168,7 +167,7 @@ def get_github_archive(url: str):
     return archive
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # url = 'https://github.com/richardanaya/js-wasm/tree/master/examples/snake'
     # save_to_path = 'plop/'
     # debug = True
@@ -186,18 +185,36 @@ if __name__ == '__main__':
 
     params = sys.argv[1:]
     for i, param in enumerate(params):
-        if param in ['-v', '--verbose']:
+        if param in ["-v", "--verbose"]:
             verbose = True
-        elif param in ['-d', '--debug']:
+        elif param in ["-d", "--debug"]:
             debug = True
-        elif param in ['-y', '--yes']:
+        elif param in ["-y", "--yes"]:
             yes_man = True
-        elif param in ['-n', '--no']:
+        elif param in ["-n", "--no"]:
             no_no_no_no = True
-        elif param in ['-t', '--tar']:
+        elif param in ["-t", "--tar"]:
             only_save_tar = True
+        elif param in ["-h", "--help"]:
+            print(
+                cleandoc(
+                    """
+                    github <ARGUMENTS> <github_url>
 
-        if param.startswith('-'):
+                    ARGUMENTS
+                    =========
+                    -v --verbose : More verbose.
+                    -d --debug : Debug verbosity. Load debug.tar.gz if it exists unless dowload the archive of the link and save it in debug.tar.gz
+                    -y --yes : Answer yes to all overriding questions.
+                    -n --no : Answer no to all overriding questions.
+                    -t --tar : download the tar extract the corresponding path to another tar without extracting it.
+                    -h --help : show the help.
+                    """
+                )
+            )
+            exit(0)
+
+        if param.startswith("-"):
             pass
         elif url is None:
             url = param
@@ -205,17 +222,17 @@ if __name__ == '__main__':
             save_to_path = param
 
     if url is None:
-        print('Required parameter : url')
+        print("Required parameter : url ; use -h for --help")
         exit(0)
     elif save_to_path is None:
-        save_to_path = '.'
+        save_to_path = "."
     PurePath(save_to_path)  # check path is valid
 
     if debug:
-        print(f'{debug=}')
-        print(f'{verbose=}')
-        print(f'{yes_man=}')
-        print(f'{no_no_no_no=}')
+        print(f"{debug=}")
+        print(f"{verbose=}")
+        print(f"{yes_man=}")
+        print(f"{no_no_no_no=}")
 
     archive = get_github_archive(url)
     archive.extract(directory=save_to_path)
